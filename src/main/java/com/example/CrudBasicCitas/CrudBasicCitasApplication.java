9c262fde9ed8a6dbd2663cf250a9d54909fe9cdf
cdelriot1121 <carandresdelrio1121@gmail.com>
2025-09-03 12:45:01 -0500

    First Commit

---
+++ src/main/java/com/example/CrudBasicCitas/CrudBasicCitasApplication.java
@@ -0,0 +1,13 @@
+package com.example.CrudBasicCitas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudBasicCitasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudBasicCitasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/CrudBasicCitas/CrudBasicCitasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CrudBasicCitas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudBasicCitasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
